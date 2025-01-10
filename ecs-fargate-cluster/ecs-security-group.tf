2e24bf47082c95ec9b7106f5e5a3480579dfa11f
hbekal005 <hbekal005@gmail.com>
2025-01-10 00:47:38 -0500

    Updated the code for lb error fix

--- ecs-fargate-cluster/ecs-security-group.tf
+++ ecs-fargate-cluster/ecs-security-group.tf
@@ -19,6 +19,14 @@ resource "aws_security_group" "ecs-security-group" {
     cidr_blocks = [var.all_cidr_block]
   }
 
+  # Allow inbound traffic on port 8080 for the ECS tasks
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.all_cidr_block]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
--- ecs-fargate-cluster/vpc.tf
+++ ecs-fargate-cluster/vpc.tf
@@ -28,14 +28,14 @@ resource "aws_subnet" "ecs-subnet-01" {
 }
 
 resource "aws_subnet" "ecs-subnet-02" {
-  vpc_id     = aws_vpc.conversion-app-vpc.id
-  cidr_block = cidrsubnet(var.vpc_cidr_block[terraform.workspace], var.subnet_mask, var.subnet_count + 1)
+  vpc_id                  = aws_vpc.conversion-app-vpc.id
+  cidr_block              = cidrsubnet(var.vpc_cidr_block[terraform.workspace], var.subnet_mask, var.subnet_count + 1)
+  availability_zone       = element(data.aws_availability_zones.ecs-available-zones.names, 1)
+  map_public_ip_on_launch = true
   tags = {
     Name = "${terraform.workspace}-ecs-subnet-02"
   }
 }
-
-
 
 
 # Creating an internet gateway
@@ -47,54 +47,34 @@ resource "aws_internet_gateway" "conversion-app-igw" {
   }
 }
 
-resource "aws_eip" "elastic-ip" {
-  domain = "vpc"
-}
-
-resource "aws_nat_gateway" "conversion-app-nat-gw" {
-  allocation_id = aws_eip.elastic-ip.id
-  subnet_id     = aws_subnet.ecs-subnet-01.id
-  tags = {
-    Name = "${terraform.workspace}-conversion-app-igw"
-  }
-}
 
 
 # Creating a ECS Route Table
-resource "aws_route_table" "ecs-route-table-01" {
+resource "aws_route_table" "ecs-route-table" {
   vpc_id = aws_vpc.conversion-app-vpc.id
   tags = {
-    Name = "${terraform.workspace}-ecs-route-table-01"
+    Name = "${terraform.workspace}-ecs-route-table"
   }
 }
 
-resource "aws_route_table" "ecs-route-table-02" {
-  vpc_id = aws_vpc.conversion-app-vpc.id
-  tags = {
-    Name = "${terraform.workspace}-ecs-route-table-01"
-  }
-}
+
 
 # Create Routes for the ECS Route Table
-resource "aws_route" "ecs-route-01" {
-  route_table_id         = aws_route_table.ecs-route-table-01.id
+resource "aws_route" "ecs-route" {
+  route_table_id         = aws_route_table.ecs-route-table.id
   destination_cidr_block = var.all_cidr_block
   gateway_id             = aws_internet_gateway.conversion-app-igw.id
 }
 
-resource "aws_route" "ecs-route-02" {
-  route_table_id         = aws_route_table.ecs-route-table-02.id
-  destination_cidr_block = var.all_cidr_block
-  gateway_id             = aws_nat_gateway.conversion-app-nat-gw.id
-}
+
 
 #Route Table Association
 resource "aws_route_table_association" "rtb-subnet-association-01" {
   subnet_id      = aws_subnet.ecs-subnet-01.id
-  route_table_id = aws_route_table.ecs-route-table-01.id
+  route_table_id = aws_route_table.ecs-route-table.id
 }
 
 resource "aws_route_table_association" "rtb-subnet-association-02" {
   subnet_id      = aws_subnet.ecs-subnet-02.id
-  route_table_id = aws_route_table.ecs-route-table-02.id
+  route_table_id = aws_route_table.ecs-route-table.id
 }
